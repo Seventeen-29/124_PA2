c6fd635f026711135d5e0ebf7f59bebbe5adac01
rakesh-nori <20rakeshn@students.harker.org>
2021-03-21 13:38:49 -0400

    finished task 2 and task 3

--- strassen.cpp
+++ strassen.cpp
@@ -60,11 +60,11 @@ void genRandMats(int low, int high, int n, string fileName){
 //    NOTE: assumes `a`, `b`, and `c` are square matrices with the same dimensions.
 //    Computes the matrix product `a * b` and stores it in `c` through the conventional method.
 //    Inspired by Eddie Kohler's Network 8 Lecture from CS 61
-vector<vector<int>> matrix_multiply(vector<vector<int>> &a, vector<vector<int>> &b) {
+vector<vector<int> > matrix_multiply(vector<vector<int> > &a, vector<vector<int> > &b) {
 
     int n = a.size();
 
-    vector<vector<int>> c(n , vector<int> (n, 0)); 
+    vector<vector<int> > c(n , vector<int> (n, 0)); 
 
     for (size_t i = 0; i < n; ++i){
         vector<int> currRowA = a[i];
@@ -80,8 +80,8 @@ vector<vector<int>> matrix_multiply(vector<vector<int>> &a, vector<vector<int>> 
     return c;
 }
 
-vector<vector<int>> top_left(vector<vector<int>> m){
-    vector<vector<int>> out;
+vector<vector<int> > top_left(vector<vector<int> > m){
+    vector<vector<int> > out;
     for(int row = 0; row < m.size() / 2; row++){
         vector<int> temp;
         for(int col = 0; col < m.size() / 2; col++){
@@ -92,8 +92,8 @@ vector<vector<int>> top_left(vector<vector<int>> m){
     return out;
 }
 
-vector<vector<int>> bottom_left(vector<vector<int>> m){
-    vector<vector<int>> out;
+vector<vector<int> > bottom_left(vector<vector<int> > m){
+    vector<vector<int> > out;
     for(int row = m.size() / 2; row < m.size(); row++){
         vector<int> temp;
         for(int col = 0; col < m.size() / 2; col++){
@@ -104,8 +104,8 @@ vector<vector<int>> bottom_left(vector<vector<int>> m){
     return out;
 }
 
-vector<vector<int>> top_right(vector<vector<int>> m){
-    vector<vector<int>> out;
+vector<vector<int> > top_right(vector<vector<int> > m){
+    vector<vector<int> > out;
     for(int row = 0; row < m.size() / 2; row++){
         vector<int> temp;
         for(int col = m.size() / 2; col < m.size(); col++){
@@ -116,8 +116,8 @@ vector<vector<int>> top_right(vector<vector<int>> m){
     return out;
 }
 
-vector<vector<int>> bottom_right(vector<vector<int>> m){
-    vector<vector<int>> out;
+vector<vector<int> > bottom_right(vector<vector<int> > m){
+    vector<vector<int> > out;
     for(int row = m.size() / 2; row < m.size(); row++){
         vector<int> temp;
         for(int col = m.size() / 2; col < m.size(); col++){
@@ -128,7 +128,7 @@ vector<vector<int>> bottom_right(vector<vector<int>> m){
     return out;
 }
 
-void print_mtx(vector<vector<int>> m){
+void print_mtx(vector<vector<int> > m){
     for(vector<int> row : m){
         for(int col : row){
             cout << col << " ";
@@ -137,8 +137,14 @@ void print_mtx(vector<vector<int>> m){
     }
 }
 
-vector<vector<int>> add(vector<vector<int>> m, vector<vector<int>> n){
-    vector<vector<int>> out;
+void print_diag(vector<vector<int> > m, int dim){
+    for(int i = 0; i < dim; ++i){
+        cout << i << " " << m[i][i] << endl;
+    }
+}
+
+vector<vector<int> > add(vector<vector<int> > m, vector<vector<int> > n){
+    vector<vector<int> > out;
     for(int row = 0; row < m.size(); row++){
         vector<int> temp;
         for(int col = 0; col < m.size(); col++){
@@ -149,8 +155,8 @@ vector<vector<int>> add(vector<vector<int>> m, vector<vector<int>> n){
     return out;
 }
 
-vector<vector<int>> sub(vector<vector<int>> m, vector<vector<int>> n){
-    vector<vector<int>> out;
+vector<vector<int> > sub(vector<vector<int> > m, vector<vector<int> > n){
+    vector<vector<int> > out;
     for(int row = 0; row < m.size(); row++){
         vector<int> temp;
         for(int col = 0; col < m.size(); col++){
@@ -161,9 +167,9 @@ vector<vector<int>> sub(vector<vector<int>> m, vector<vector<int>> n){
     return out;
 }
 
-vector<vector<int>> join(vector<vector<int>> topLeft, vector<vector<int>> topRight,
-    vector<vector<int>> bottomLeft, vector<vector<int>> bottomRight){
-        vector<vector<int>> out;
+vector<vector<int> > join(vector<vector<int> > topLeft, vector<vector<int> > topRight,
+    vector<vector<int> > bottomLeft, vector<vector<int> > bottomRight){
+        vector<vector<int> > out;
         for(int row = 0; row < topLeft.size(); row++){
             vector<int> tempRow(topLeft.size() * 2, 0);
             for(int col = 0; col < topLeft.size(); col++){
@@ -183,9 +189,9 @@ vector<vector<int>> join(vector<vector<int>> topLeft, vector<vector<int>> topRig
         return out;
     }
 
-vector<vector<int>> pad(vector<vector<int>> m){
+vector<vector<int> > pad(vector<vector<int> > m){
     int extraZeros = ((int) pow(2, ceil(log2(m.size())))) - m.size();
-    vector<vector<int>> out;
+    vector<vector<int> > out;
     for(int row = 0; row < m.size(); row++){
         vector<int> tempRow;
         for(int i = 0; i < m.size(); i++){
@@ -202,36 +208,36 @@ vector<vector<int>> pad(vector<vector<int>> m){
     return out;
 }
 
-vector<vector<int>> strassen_multiply(vector<vector<int>> m, vector<vector<int>> n, int thresh){
+vector<vector<int> > strassen_multiply(vector<vector<int> > m, vector<vector<int> > n, int thresh){
     if(m.size() < thresh){
         
         return matrix_multiply(m, n);
         //conventional
     }
     else{
-        vector<vector<int>> a = top_left(m);
-        vector<vector<int>> b = top_right(m);
-        vector<vector<int>> c = bottom_left(m);
-        vector<vector<int>> d = bottom_right(m);
-        vector<vector<int>> e = top_left(n);
-        vector<vector<int>> f = top_right(n);
-        vector<vector<int>> g = bottom_left(n);
-        vector<vector<int>> h = bottom_right(n);
+        vector<vector<int> > a = top_left(m);
+        vector<vector<int> > b = top_right(m);
+        vector<vector<int> > c = bottom_left(m);
+        vector<vector<int> > d = bottom_right(m);
+        vector<vector<int> > e = top_left(n);
+        vector<vector<int> > f = top_right(n);
+        vector<vector<int> > g = bottom_left(n);
+        vector<vector<int> > h = bottom_right(n);
 
        // print_mtx(add(a, b));
 
-        vector<vector<int>> p1 = strassen_multiply(a, sub(f, h), thresh);
-        vector<vector<int>> p2 = strassen_multiply(add(a, b), h, thresh);
-        vector<vector<int>> p3 = strassen_multiply(add(c, d), e, thresh);
-        vector<vector<int>> p4 = strassen_multiply(d, sub(g, e), thresh);
-        vector<vector<int>> p5 = strassen_multiply(add(a, d), add(e, h), thresh);
-        vector<vector<int>> p6 = strassen_multiply(sub(b, d), add(g, h), thresh);
-        vector<vector<int>> p7 = strassen_multiply(sub(a, c), add(e, f), thresh);
+        vector<vector<int> > p1 = strassen_multiply(a, sub(f, h), thresh);
+        vector<vector<int> > p2 = strassen_multiply(add(a, b), h, thresh);
+        vector<vector<int> > p3 = strassen_multiply(add(c, d), e, thresh);
+        vector<vector<int> > p4 = strassen_multiply(d, sub(g, e), thresh);
+        vector<vector<int> > p5 = strassen_multiply(add(a, d), add(e, h), thresh);
+        vector<vector<int> > p6 = strassen_multiply(sub(b, d), add(g, h), thresh);
+        vector<vector<int> > p7 = strassen_multiply(sub(a, c), add(e, f), thresh);
         
-        vector<vector<int>> topLeft = add(sub(add(p5, p4), p2),p6);
-        vector<vector<int>> topRight = add(p1, p2);
-        vector<vector<int>> bottomLeft = add(p3, p4);
-        vector<vector<int>> bottomRight = sub(add(p5, p1), add(p3, p7));
+        vector<vector<int> > topLeft = add(sub(add(p5, p4), p2),p6);
+        vector<vector<int> > topRight = add(p1, p2);
+        vector<vector<int> > bottomLeft = add(p3, p4);
+        vector<vector<int> > bottomRight = sub(add(p5, p1), add(p3, p7));
 
         return join(topLeft, topRight, bottomLeft, bottomRight);
 
@@ -252,17 +258,50 @@ vector<vector<int>> strassen_multiply(vector<vector<int>> m, vector<vector<int>>
 
 int main(int argc, char *argv[]){	
     srand((unsigned)time(NULL));
+    srand48((unsigned)time(NULL));
     int flag = stoi(argv[1]);
     int dim = stoi(argv[2]);
     string fileName = argv[3];
 
-    genRandMats(0, 2, dim, fileName.c_str());
+    //cout << "FLAG " << flag << endl;
 
+    // option to generate random matrices on the fly
+    if (flag == 1)
+        genRandMats(0, 2, dim, fileName.c_str()); // generates random ints from 0 to 2 (inclusive)
+    
+
+    // task 3: flag is the probability
+    if (flag == 2){
+        int vertices = 1024;
+        cout << "type value of p: " << endl;
+        double p;
+        cin >> p;
+        vector<vector<int> > A(vertices, vector<int> (vertices, 0));
+        for (int i = 0; i < vertices; i++)
+        {
+            for (int j = 0; j < i; j++)
+            {
+                if (drand48() < p){
+                    A[i][j] = 1;
+                    A[j][i] = 1;
+                }
+            }
+        }
+        //print_mtx(A);
+        vector<vector<int> > res = strassen_multiply(strassen_multiply(A, A, 512), A, 512);
+        int sum = 0;
+        for (int i = 0; i < res.size(); i++){
+            sum += res[i][i];
+        }
+        //print_diag(strassen_multiply(strassen_multiply(A, A, 512), A, 512));
+        cout << "NUM TRIANGLES: " << (sum / 6) << endl;
+        return 0;
+    }
     fstream file; 
     file.open(fileName.c_str()); 
 
-    vector<vector<int>> m(dim , vector<int> (dim));
-    vector<vector<int>> n(dim , vector<int> (dim));
+    vector<vector<int> > m(dim , vector<int> (dim));
+    vector<vector<int> > n(dim , vector<int> (dim));
     int curr;
     
     for (int i = 0; i < dim; i++){
@@ -283,30 +322,68 @@ int main(int argc, char *argv[]){
     m = pad(m);
     n = pad(n);
     
-    cout << "Matrix 1: " << endl;
+    //cout << "Matrix 1: " << endl;
     //print_mtx(m);
-    cout << "Matrix 2: " << endl; 
+    //cout << "Matrix 2: " << endl; 
     //print_mtx(n);
 
-    int thresh = 5;
+    // for testing optimal n_0
+    if (flag == 1){
+        vector<int> thresh;
+        thresh.push_back(256);
+        thresh.push_back(512);
+        thresh.push_back(768);
+        thresh.push_back(896);
+        thresh.push_back(1024);
+        thresh.push_back(1152);
+        thresh.push_back(1280);
+        thresh.push_back(1536);
+        thresh.push_back(2048);
+        //thresh = { 256, 512, 768, 896, 1024, 1152, 1280, 1536, 2048 };
 
-    /**
-    while(thresh <= 1200){
-        auto start = chrono::high_resolution_clock::now(); 
-        print_mtx(strassen_multiply(m, n, thresh));
-        auto stop = chrono::high_resolution_clock::now(); 
-	    auto duration = chrono::duration_cast<chrono::milliseconds>(stop - start); 
-        cout << "DURATION for strassen: " << duration.count() << " ms" << endl; 
-        thresh += 100;
+        for (int elem : thresh){
+
+            auto start = chrono::high_resolution_clock::now(); 
+            strassen_multiply(m, n, elem);
+            //print_mtx(strassen_multiply(m, n, thresh));
+            auto stop = chrono::high_resolution_clock::now(); 
+            auto duration = chrono::duration_cast<chrono::milliseconds>(stop - start); 
+            cout << elem << " DURATION for strassen: " << duration.count() << " ms" << endl;
+        }
+    }
+    else if (flag == 0){
+        print_diag(strassen_multiply(m, n, 512), dim);
     }
 
-    **/
-
+/**
     auto start = chrono::high_resolution_clock::now(); 
-    strassen_multiply(m, n, thresh);
+    strassen_multiply(m, n, thresh0);
     //print_mtx(strassen_multiply(m, n, thresh));
     auto stop = chrono::high_resolution_clock::now(); 
 	auto duration = chrono::duration_cast<chrono::milliseconds>(stop - start); 
     cout << "DURATION for strassen: " << duration.count() << " ms" << endl;
+
+    start = chrono::high_resolution_clock::now(); 
+    strassen_multiply(m, n, thresh1);
+    //print_mtx(strassen_multiply(m, n, thresh));
+    stop = chrono::high_resolution_clock::now(); 
+    duration = chrono::duration_cast<chrono::milliseconds>(stop - start); 
+    cout << "DURATION for strassen: " << duration.count() << " ms" << endl;
+
+    start = chrono::high_resolution_clock::now(); 
+    strassen_multiply(m, n, thresh2);
+    //print_mtx(strassen_multiply(m, n, thresh));
+    stop = chrono::high_resolution_clock::now(); 
+	duration = chrono::duration_cast<chrono::milliseconds>(stop - start); 
+    cout << "DURATION for strassen: " << duration.count() << " ms" << endl;
+
+    start = chrono::high_resolution_clock::now(); 
+    strassen_multiply(m, n, thresh3);
+    //print_mtx(strassen_multiply(m, n, thresh));
+    stop = chrono::high_resolution_clock::now(); 
+	duration = chrono::duration_cast<chrono::milliseconds>(stop - start); 
+    cout << "DURATION for strassen: " << duration.count() << " ms" << endl;
+
+    **/
 
 }
